40c17676f5a05432c362afb6efdd6f76cdce7ca1
E Madeshwaran <114077739+madesh405@users.noreply.github.com>
2025-08-30 00:51:45 +0530

    success of gui

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        Client client=new Client();
+        client.startGUI();
+    }
+}
